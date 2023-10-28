3bc79bda268002805e60787c8f14a8ff2e54e008
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-10-28 16:23:59 +0530

    Update LevelOrderTraversal.py

--- Tree/LevelOrderTraversal.py
+++ Tree/LevelOrderTraversal.py
@@ -1,3 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode):
+        ans = []
+        
+        if not root:
+            return ans
+            
+        q = [root]
+        
+        while q:
+            temp = q.pop(0)
+            
+            if temp.left:
+                q.append(temp.left)
+            if temp.right:
+                q.append(temp.right)
+                
+            ans.append(temp.val)
+        
+        return ans
+
+
+
+.................................................CPP............................................
 class Solution {
 public:
     vector<int> levelOrder(TreeNode* root) {
