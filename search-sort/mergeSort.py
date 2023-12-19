eb64b1798b8e43384556298167ed3e8641e87769
Nakula Gowda <91659838+nakulamk@users.noreply.github.com>
2023-12-19 12:39:11 +0530

    Update mergeSort.py

--- search-sort/mergeSort.py
+++ search-sort/mergeSort.py
@@ -43,3 +43,41 @@ ms(lb, ub, a)
 
 # Print the sorted array
 print("Sorted Array:", a)
+
+
+
+
+# Let's perform a dry run of the provided Merge Sort code step by step using the example array `[8, 1, 15, 5, 24]`. The goal is to illustrate how the array is split into smaller segments and then merged back together in a sorted manner.
+
+# ### Initial State:
+
+# - Original Array: `[8, 1, 15, 5, 24]`
+# - lb (lower bound): 0
+# - ub (upper bound): 4
+
+# ### Initial Call to `ms(lb, ub, a)`:
+
+# 1. **First Recursive Call:**
+#    - `ms(0, 2, a)` (left half of the array)
+#      - Split `[8, 1, 15]`
+#      - `ms(0, 1, a)` (left half of the left half)
+#        - Split `[8, 1]`
+#        - `ms(0, 0, a)` (single element, no further split)
+#        - `ms(1, 1, a)` (single element, no further split)
+#        - Merge `[8]` and `[1]` into sorted `[1, 8]`
+#      - `ms(2, 2, a)` (single element, no further split)
+#      - Merge sorted `[1, 8]` and `[15]` into sorted `[1, 8, 15]`
+
+# 2. **Second Recursive Call:**
+#    - `ms(3, 4, a)` (right half of the array)
+#      - Split `[5, 24]`
+#      - `ms(3, 3, a)` (single element, no further split)
+#      - `ms(4, 4, a)` (single element, no further split)
+#      - Merge sorted `[5]` and `[24]` into sorted `[5, 24]`
+
+# 3. **Final Merge:**
+#    - Merge sorted `[1, 8, 15]` and `[5, 24]` into the final sorted array `[1, 5, 8, 15, 24]`
+
+# ### Final Sorted Array:
+
+# The final sorted array is `[1, 5, 8, 15, 24]`, which is the correct result of applying the Merge Sort algorithm to the initial array `[8, 1, 15, 5, 24]`.
